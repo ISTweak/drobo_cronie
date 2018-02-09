daa809de9cac7144317c142f2e7dbe11bca43134
ISTweak <istweak@d-o-b.org>
2018-02-09 10:32:09 +0900

    update 1.5.1

--- app.sh
+++ app.sh
@@ -1,12 +1,14 @@
 ### RSYNC ###
 _build_cronie() {
-local VERSION="1.4.12"
-local FOLDER="cronie${VERSION}"
+local VERSION="1.5.1"
+local FOLDER="cronie-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
 local URL="https://github.com/cronie-crond/cronie/archive/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/cronie-${FOLDER}"
+
+patch -u src/popen.c < ../../src/popen.patch
 
 #autoconf
 autoreconf --install
